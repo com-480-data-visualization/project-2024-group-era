1cdad77e9f1d5b505cabda7831422afce556facb
JiananAlvin <jiananwang990@gmail.com>
2024-04-20 20:28:35 +0200

    Add 3D globe

--- frontend/src/components/GlobeSection.jsx
+++ frontend/src/components/GlobeSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Globe from 'react-globe.gl'
 
 const GlobeSection = () => {
   return (
@@ -7,6 +8,14 @@ const GlobeSection = () => {
         <p className="mt-10 text-lg text-center max-w-4xl">
           3D Globe Navigation: Explore the world and discover the distribution of endangered species.
         </p>
+        {/* Globe Component Added Below */}
+          <Globe
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+            bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+            backgroundColor="rgba(0,0,0,0)" // makes the canvas background transparent
+            width={900}  // Width of the globe canvas
+            height={900} // Height of the globe canvas
+          />
       </div>
     </div>
   )
--- frontend/src/components/HabitatsSection.jsx
+++ frontend/src/components/HabitatsSection.jsx
@@ -5,7 +5,7 @@ const HabitatsSection = () => {
     <div className="min-h-screen">
       <div className="flex flex-col items-center mt-6 lg:mt-20">
         <p className="mt-10 text-lg text-center max-w-4xl">
-          3D Globe Navigation
+          Habitats: Discover the ecosystems where endangered species live and learn about the threats they face. 
         </p>
       </div>
     </div>
