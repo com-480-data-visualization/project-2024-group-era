5a8230ea9395f26c79e90cac841563cddba74568
JiananAlvin <jiananwang990@gmail.com>
2024-04-25 21:36:37 +0200

    put measures packed circles in a container

--- frontend/src/components/measures/CircularPacking.tsx
+++ frontend/src/components/measures/CircularPacking.tsx
@@ -6,9 +6,8 @@ type CircularPackingProps = {
   width: number;
   height: number;
   data: Tree;
+  legendWidth: number;
 };
-
-const MARGIN = 3;
 
 const colors = [
   "#e0ac2b",
@@ -24,118 +23,63 @@ export const CircularPacking = ({
   width,
   height,
   data,
+  legendWidth
 }: CircularPackingProps) => {
-    const hierarchy = d3
-        .hierarchy(data)
-        .sum((d) => d.value)
-        .sort((a, b) => b.value! - a.value!);
+    const hierarchy = d3.hierarchy(data)
+                        .sum(d => d.value)
+                        .sort((a, b) => b.value - a.value);
 
     const packGenerator = d3.pack<Tree>().size([width, height]).padding(4);
     const root = packGenerator(hierarchy);
 
-    // List of item of level 1 (just under root) & related color scale
-    const firstLevelGroups = hierarchy?.children?.map((child) => child.data.name);
-    var colorScale = d3
-        .scaleOrdinal<string>()
-        .domain(firstLevelGroups || [])
-        .range(colors);
+    const colorScale = d3.scaleOrdinal()
+                         .domain(hierarchy.children?.map(child => child.data.name) || [])
+                         .range(colors);
 
-    // Mapping the smallest value to 0.2 opacity and the largest to 1
-    const leaves = root.leaves();
-    const valueExtent = d3.extent(leaves, leaf => leaf.value) as [number, number];
+    const valueExtent = d3.extent(root.leaves(), leaf => leaf.value) as [number, number];
     const opacityScale = d3.scaleLinear()
                            .domain(valueExtent)
-                           .range([0.2, 1]); 
+                           .range([0.2, 1]);
 
-    // Circles for level 1 of the hierarchy
-    const allLevel1Circles = root
-        .descendants()
-        .filter((node) => node.depth === 1)
-        .map((node) => {
-        const parentName = node.data.name;
-
-        return (
-            <g key={node.data.name}>
-            <circle
-                cx={node.x}
-                cy={node.y}
-                r={node.r}
-                stroke={colorScale(parentName)}
-                strokeWidth={1}
-                strokeOpacity={0.3}
-                fill={colorScale(parentName)}
-                fillOpacity={0.1}
-            />
-            </g>
-        );
-        });
-
-    // Circles for level 2 = leaves
-    const allLeafCircles = root.leaves().map((leaf) => {
-        const parentName = leaf.parent?.data.name;
-
-        if (!parentName) {
-        return null;
-        }
-
-        return (
-        <g key={leaf.data.name}>
-            <circle
-                cx={leaf.x}
-                cy={leaf.y}
-                r={leaf.r}
-                stroke={colorScale(parentName)}
-                strokeWidth={2}
-                fill={colorScale(parentName)}
-                fillOpacity={opacityScale(leaf.value)}
-            />
-
-            <text
-                key={leaf.data.name}
-                x={leaf.x}
-                y={leaf.y}
-                fontSize={13}
-                fontWeight={0.4}
-                dominant-baseline="middle"
-                text-anchor="middle"
-            >
-                {leaf.data.value}
-            </text>
-        </g>
-        );
-    });
-
-    // Function to create legends based on nodes
-    const createLegends = (nodes) => {
-        return nodes.map((node, index) => {
-            const parentName = node.parent?.data.name;
-            const isLeaf = !node.children;
-            return (
-                <g key={node.data.name} transform={`translate(0, ${index * 20})`}>
-                <rect width="18" height="18" fill={colorScale(isLeaf ? parentName : node.data.name)} fillOpacity={isLeaf ? opacityScale(node.value) : 0.1}/>
+    const createLegendItems = (nodes) => {
+        return nodes.map((node, index) => (
+            <g key={node.data.name} transform={`translate(0, ${index * 20})`}>
+                <rect width="18" height="18" fill={node.children ? colorScale(node.data.name) : colorScale(node.parent?.data.name)} fillOpacity={node.children ? 0.1 : opacityScale(node.value)}/>
                 <text x="24" y="9" dy="0.35em" fill="white">{node.data.name}</text>
-                </g>
-            );
-        });
+            </g>
+        ));
     };
 
-    // Calculate the total width including legends
-    const legendWidth = 200; // adjust as needed for text length
-    const totalWidth = width + legendWidth;
-
-    // Generate the legend data
-    // Assuming the legends are for the first level nodes and leaves
-    const firstLevelNodesAndLeaves = root.descendants().filter(d => d.depth === 1 || d.depth === root.height);
-
-    const legends = createLegends(firstLevelNodesAndLeaves);
+    const legends = createLegendItems(hierarchy.descendants().filter(d => d.depth === 1 || d.depth === root.height));
 
     return (
-        <svg width={3 * width} height={height} style={{ display: "inline-block" }}>
-            <g transform={`translate(${legendWidth}, 0)`}>
-                {allLevel1Circles}
-                {allLeafCircles}
+        <svg width={width + legendWidth} height={height} style={{ display: "inline-block" }}>
+            <g transform={`translate(0, 0)`}>
+                {root.descendants().filter(node => node.depth > 0).map(node => (  // Filter out the root node
+                    <g key={node.data.name} transform={`translate(${node.x}, ${node.y})`}>
+                        <circle
+                            r={node.r}
+                            stroke={colorScale(node.parent?.data.name)}
+                            strokeWidth={1}
+                            strokeOpacity={0.3}
+                            fill={node.children ? colorScale(node.data.name) : colorScale(node.parent?.data.name)}
+                            fillOpacity={node.children ? 0.1 : opacityScale(node.value)}
+                        />
+                        {!node.children && ( // Display the value of the node if it is a leaf node
+                            <text
+                                fontSize="13"
+                                fontWeight="400"
+                                textAnchor="middle"
+                                dominantBaseline="middle"
+                                fill="white"
+                            >
+                                {node.value}
+                            </text>
+                        )}
+                    </g>
+                ))}
             </g>
-            <g transform={`translate(${width + 100}, ${height / 4})`}>
+            <g transform={`translate(${width + 20}, ${height / 4})`}>
                 {legends}
             </g>
         </svg>
--- frontend/src/components/measures/MeasuresSection.jsx
+++ frontend/src/components/measures/MeasuresSection.jsx
@@ -1,19 +1,23 @@
 import { CircularPacking } from "./CircularPacking";
 import { data } from "./MeasuresData";
 
-const MeasuresSection = ({ width = 700, height = 400 }) => {
-    if (width === 0) {
-        return null;
-    }
-
+const MeasuresSection = () => {
     return (
-        <div id="actions" className="min-h-screen pt-24">
-            <h2 className="text-3xl font-semibold text-center mt-10">Conservation Actions</h2>
-            <div className="flex flex-col items-center ]lg:mt-20">
-                <p className="mt-10 text-lg text-center max-w-4xl">
+        <div id="conservation" className="mx-14">
+            <div className="flex flex-col items-center py-20 rounded-3xl bg-neutral-800">
+                <h1 className="text-4xl font-bold text-white mb-10 lg:mb-5 w-full text-center">
                     Discover the conservation measures taken to protect endangered species.
-                </p>
-                <CircularPacking width={width} height={height} data={data} />
+                </h1>
+                <div className="flex flex-col lg:flex-row items-center justify-center w-full">
+                    <div className="w-full lg:w-1/2 px-6">
+                        <p className="text-lg text-white">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                    <div className="w-full lg:w-1/2 px-6 flex items-center justify-center">
+                        <CircularPacking width={700} height={500} data={data} legendWidth={360}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
