e8249cde5c5f6f1dd0a721597c6a6c97cbd6dd72
Adriana Orellana <adriana.orellana.torrico@gmail.com>
2024-05-08 15:21:46 +0200

    Add descriptions to conservation chart

--- frontend/src/components/ConservationSection.jsx
+++ frontend/src/components/ConservationSection.jsx
@@ -10,35 +10,66 @@ const ConservationSection = () => {
     { category: 'Extinct', counts: 9, color: '#9D0208' },
   ];
 
+  const getDescriptionOfCategory = (label) => {
+    if (label === 'Near Threatened') {
+      return 'Species that may be considered threatened with extinction in the near future.';
+    }
+    if (label === 'Vulnerable') {
+      return 'Species facing a high risk of extinction in the wild.';
+    }
+    if (label === 'Endangered') {
+      return 'Species at risk of extinction due to factors such as habitat loss, hunting, or disease.';
+    }
+    if (label === 'Critically Endangered') {
+      return 'Species facing an extremely high risk of extinction in the immediate future.';
+    }
+    if (label === 'Extinct') {
+      return 'Species that no longer exist in the wild or in captivity.';
+    }
+      return '';
+  };
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`Status: ${label}`}</p>
+          <p>Number of species: {payload[0].value}</p>
+          <p>{getDescriptionOfCategory(label)}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
-    <div id="conservation" className="mx-14 mb-8">
-      <div className="flex flex-col items-center py-20 rounded-3xl bg-neutral-800">
-        <h1 className="text-4xl font-bold">Do you know the conservation status of species?</h1>
-        <div className="flex flex-col lg:flex-row items-center justify-center mt-10 lg:mt-20">
-          <div className="w-full lg:w-1/2">
-            <p className="mx-20">
-              In this section, we use a bar chart to represent how many animals belong to each conservation category, highlighting the number of species that are close to facing extinction. The more intense the red color, the higher the likelihood of extinction for those species. The chart in this section aims to reflect the importance of addressing immediate conservation actions.
-            </p>
-          </div>
-          <div className="w-full lg:w-1/2 px-10">
-              <ResponsiveContainer height={400}>
-                <BarChart data={data}>
-                  <CartesianGrid stroke="#333" strokeDasharray="5 5" />
-                  <XAxis dataKey="category" stroke="#fff" tick={{ fontSize: 12 }}>
-                    <Label value="Conservation Status" offset={-5} position="insideBottom" fill="#fff" />
-                  </XAxis>
-                  <YAxis stroke="#fff" tick={{ fontSize: 12 }}>
-                    <Label value="Number of Species" angle={-90} offset={10} position="insideLeft" fill="#fff" />
-                  </YAxis>
-                  <Tooltip wrapperStyle={{ backgroundColor: '#333', color: '#fff' }} />
-                  <Bar dataKey="counts" fill="#8884d8" label={{ position: 'top' }}>
-                    {data.map((entry, index) => (
-                      <Cell key={index} fill={entry.color} />
-                    ))}
-                  </Bar>
-                </BarChart>
-              </ResponsiveContainer>
-          </div>
+    <div id="conservation" className="section-container">
+      <h1 className="title">Do you know the conservation status of species?</h1>
+      <div className="flex flex-col lg:flex-row items-center justify-center mt-10 lg:mt-20">
+        <div className="w-full lg:w-1/2">
+          <p className="mx-20 text-justify">
+            Every year the number of species that are threatened with extinction increases. The International Union for Conservation of Nature (IUCN) has developed a system to classify the conservation status of species, where each category represents the risk of extinction. The categories are: Least Concern, Near Threatened, Vulnerable, Endangered, Critically Endangered, Extinct in the Wild, and Extinct. The graph presented in this section shows the number of species in each of these categories.
+          </p>
+        </div>
+        <div className="chart-container">
+            <ResponsiveContainer height={400}>
+              <BarChart data={data}>
+                <CartesianGrid stroke="#333" strokeDasharray="5 5" />
+                <XAxis dataKey="category" stroke="#fff" tick={{ fontSize: 12 }}>
+                  <Label value="Conservation Status" offset={-5} position="insideBottom" fill="#fff" />
+                </XAxis>
+                <YAxis stroke="#fff" tick={{ fontSize: 12 }}>
+                  <Label value="Number of Species" angle={-90} offset={10} position="insideLeft" fill="#fff" />
+                </YAxis>
+                <Tooltip content={<CustomTooltip />} />
+                <Bar dataKey="counts" fill="#8884d8" label={{ position: 'top' }}>
+                  {data.map((entry, index) => (
+                    <Cell key={index} fill={entry.color} />
+                  ))}
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
         </div>
       </div>
     </div>
