967d1706e3879ff3928efb06209c89c8440f5ae3
JiananAlvin <jiananwang990@gmail.com>
2024-05-23 13:08:20 +0200

    populate quiz site

--- frontend/src/components/game/quiz.jsx
+++ frontend/src/components/game/quiz.jsx
@@ -1,5 +1,8 @@
 // Reference: The sources of the questions and answers are from the following websites:
+// https://testbook.com/objective-questions/mcq-on-conservation-status--621f82b00d3ea551f10485a1
 // https://testbook.com/objective-questions/mcq-on-conservation-effort--621f82b7d94aa937799c96b2
+// https://testbook.com/objective-questions/mcq-on-ex-situ-conservation--621f8299d94aa937799c9515
+// https://testbook.com/objective-questions/mcq-on-in-situ-conservation--621f82720d3ea551f1047eec
 
 export const quizzes = [
     { 
@@ -44,5 +47,41 @@ export const quizzes = [
         options: ["Great Indian Bustard", "Cuckoo", "Peacock", "More than one of the above"], 
         answer: 0, 
         hint: "The Great Indian Bustard (GIB), the State bird of Rajasthan. It is considered India’s most critically endangered bird. It is considered the flagship grassland species, representing the health of the grassland ecology. Its population is confined mostly to Rajasthan and Gujarat. The bird is under constant threats due to collision/electrocution with power transmission lines, hunting (still prevalent in Pakistan), habitat loss and alteration as a result of widespread agricultural expansion, etc."
+    },
+    { 
+        question: "Genetic diversity in agricultural crops is threatened by", 
+        options: ["Introduction of high-yielding varieties", "Intensive use of fertilizers", "Extensive intercropping", "More than one of the above"], 
+        answer: 0, 
+        hint: "To protect potentially critical features that could be utilised to combat an unanticipated future pest or adapt to the needs of the world's food supply, genetic diversity is required in agriculture. \n \n Introduction of high-yielding varieties of seeds: \n High Yielding Variety Seeds (HYV seeds) have a high-yielding capability and are resistant to insects and illnesses. These seeds are of higher grade and are a superior choice for plentiful and healthy crop output. The introduction of high-producing variants causes the rest of the species to be eliminated or cultivated to a minimum. The high-yielding variety subsequently takes over through pollination and other cultivation processes. As a result of the dominance of high-producing varieties, genetic diversity cannot be recognised."
+    },
+    { 
+        question: "Loktak lake is the only place for which kind of endangered deers?", 
+        options: ["Chital", "Barasingha", "Sambar", "Sangai"], 
+        answer: 3, 
+        hint: "The brow-antlered deer is found in Keibul Lamjao National Park in Manipur. It is largely seen over the floating biomass, locally called “phumdi” in the South-Eastern part of Loktak Lake inside the park."
+    },
+    { 
+        question: "How can viable material of endangered species be conserved?", 
+        options: ["Gene bank", "Gene pool", "Herbarium", "More than one of the above"], 
+        answer: 0, 
+        hint: "Conserving viable genetic material of endangered species is crucial for the preservation of biodiversity and the potential future reintroduction or restoration of populations. One method of doing so is through Gene banking. \n \n Gene banking, also known as cryobanking or genetic banking, involves the collection, preservation, and storage of genetic material from endangered species to ensure the conservation of their genetic diversity."
+    },
+    { 
+        question: "Which of the following would you cite as an example of ex-situ conservation being a teacher of Environmental Studies?", 
+        options: ["Zoo", "National Park", "Sanctuary", "Biosphere Reserve"], 
+        answer: 0, 
+        hint: "Ex-situ conservation refers to preserving all live species in artificial or artistic environments that closely resemble their native habitats. It is off-site conservation. It relates to artificial ecosystems. Examples: Zoos, Cryopreservation DNA banks, Aquariums etc."
+    },
+    { 
+        question: "Which one of the following is not a protected area?", 
+        options: ["Wildlife Sanctuary", "Zoological Park", "National Park", "Biosphere Reserve"], 
+        answer: 1, 
+        hint: "Protected area: \n To protect our flora and fauna and their habitats, protected areas called wildlife sanctuaries, national parks and biosphere reserves have been earmarked. Plantation, cultivation, grazing, felling trees, hunting and poaching are prohibited there. \n \n Zoological Park: \n A zoological park is defined as a place in which various animals are kept within enclosures, demonstrated to the public, and can be used for study. Wild animals are kept under human supervision in zoological parks. Animals receive appropriate care, food, and vaccinations. It is ex-situ conservation. For example, Alipore zoological gardens, Kolkata."
+    },
+    { 
+        question: "Biodiversity Hotspots are characterized on the basis of", 
+        options: ["Endemic plant and threat perception", "Species of flowering plants", "Endemic flowering plant", "More than one of the above"], 
+        answer: 0, 
+        hint: "A biodiversity hotspot is a biogeographic region with significant levels of biodiversity that is threatened by human habitation. Biodiversity hotspots have a high concentration of endemic species."
     }
 ];
