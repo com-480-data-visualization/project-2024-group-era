33ad8282748930ef44bca6e16bd39b8c6f46d404
JiananAlvin <jiananwang990@gmail.com>
2024-05-26 20:56:04 +0200

    Add % for pie chart

--- frontend/src/components/game/GamePage.jsx
+++ frontend/src/components/game/GamePage.jsx
@@ -102,6 +102,18 @@ function GamePage() {
 
         return null;
     };
+    
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * Math.PI / 180);
+        const y = cy + radius * Math.sin(-midAngle * Math.PI / 180);
+    
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
 
     // Display the certificate if all answers are correct, otherwise display the fail image
     if (submitted) {
@@ -130,7 +142,7 @@ function GamePage() {
                 </p>
                 <ResponsiveContainer width="100%" height={300}>
                     <PieChart>
-                        <Pie dataKey="value" isAnimationActive={true} data={pieData} cx="50%" cy="50%" outerRadius={100} fill="#8884d8">
+                        <Pie dataKey="value" isAnimationActive={true} data={pieData} cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label={renderCustomizedLabel}>
                             {pieData.map((entry, index) => <Cell key={`cell-${index}`} fill={index === 0 ? '#82ca9d' : '#ff726f'} />)}
                         </Pie>
                         <Legend align="center" verticalAlign="bottom" layout="horizontal" />
